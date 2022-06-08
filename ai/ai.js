e4cb332c6c5dee6bde7487f1ef23bcbc3ff169f0
R-oso <luukroso@gmail.com>
2022-06-08 15:20:42 +0200

    Initial commit

---
+++ ai/ai.js
@@ -0,0 +1,96 @@
+// The video
+let video;
+// For displaying the label
+let label = "";
+// The classifier
+let classifier;
+let modelURL = "https://teachablemachine.withgoogle.com/models/VdFvMP0WA/";
+
+let counter = 0
+
+
+// STEP 1: Load the model!
+function preload() {
+    classifier = ml5.imageClassifier(modelURL + 'model.json');
+}
+
+function setup() {
+    createCanvas(1000, 1600);
+    // Create the video
+    video = createCapture(VIDEO);
+    //     audio: false,
+    //     video: 
+    //     {
+    //       facingMode: {
+    //         exact: "environment"
+    //       } 
+    //     }
+    //   });
+    video.hide();
+    
+    // STEP 2: Start classifying
+    classifyVideo();
+}
+
+// STEP 2 classify the videeo!
+function classifyVideo() {
+    classifier.classify(video, gotResults);
+}
+
+function draw() {
+    background(0);
+    // Draw the video
+    image(video, 0, 0, 1500, 1600);
+
+    // STEP 4: Draw the label
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    fill(255);
+    text(label, width / 2, height - 16);
+
+    // If statements to check labels
+    if (label == "Geen Plant") {
+        // Do nothing, keep classifying
+    } else {
+        counter++
+        if (counter == 100) {
+            const plantText = document.createElement('p');
+            plantText.innerHTML = `Ik weet zeker dat dit een ${label} is!`;
+            document.body.appendChild(plantText);  
+
+            const canvas = document.querySelector('.p5Canvas');
+            const video = document.querySelector('video');
+            canvas.remove();
+
+            video.pause();
+            video.removeAttribute('src');
+
+            passLabel(label);
+        } 
+    } 
+}
+
+// STEP 3: Get the classification!
+function gotResults(error, results) {
+    // Something went wrong!
+    if (error) {
+        console.error(error);   
+        return;
+    }
+
+    // Store the label and classify again!
+    label = results[0].label;
+
+    if (counter > 200) {
+        console.log("stop")
+        return;
+    } else {
+        classifyVideo();
+    }    
+}
+
+function passLabel(label) {
+    let plantNaam = label;
+    localStorage.setItem("plant", plantNaam)
+    window.location.href = "/index.html"
+}
---
+++ app.js
@@ -0,0 +1,174 @@
+const url = 'https://api.thingspeak.com/channels/1747179/feeds.json?key=G47577QHO93IWHOU&results=1'
+
+// AI plant voorspelling
+let plantvoorspelling = window.localStorage.getItem("plant")
+console.log(plantvoorspelling)
+
+// sensor metingen
+// sensor grondvocht meting
+let grondvochtSensor  
+// Temperatuur data
+let temperatuurSensor  
+// plant info array 
+let perfectePlantSituatie = []
+// minimale/maximale grondvocht waardes
+let minimaleGrondvocht
+let maximaleGrondvocht
+// minimale/maximale grondvocht waardes
+let minimaletemperatuur
+let maximaletemperatuur
+
+
+let plantDataLoaded = ""
+
+// checks if there has been an update and prints message
+let message = false
+
+function getSensorData(){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => showSensorData(data))
+}
+
+function showSensorData(data){
+    temperatuurSensor = data.feeds[0].field1;
+    luchtvochtSensor = data.feeds[0].field2;
+    grondvochtSensor = data.feeds[0].field3;
+
+    console.log('Data van sensoren:')
+    console.log(grondvochtSensor);
+    console.log(temperatuurSensor);
+    console.log(luchtvochtSensor);
+    setTimeout(getSensorData, 10000);
+    if (plantDataLoaded == false){
+    loadPlantData()    
+    }
+
+    if (message == true) {
+        meldingen()
+    }
+    
+}
+
+
+// // papa parse
+function loadPlantData() {
+    Papa.parse('./plantdata.csv', {
+        download: true,
+        header: true,
+        dynamicTyping: true,
+        complete: results => searchPlant(results.data)
+    })
+}
+
+// compare fetched sensor data with plant data
+function searchPlant(data) {
+    plantDataLoaded = true
+
+    perfectePlantSituatie = []
+
+    for (let plant of data) {
+        if (plant.plantNaam === plantvoorspelling) {
+            console.log("gevonden waarden uit CSV:")
+            console.log(plant.grondvocht)
+            console.log(plant.temperatuur)
+
+            perfectePlantSituatie = [plant.grondvocht, plant.temperatuur]
+            minimaleGrondvocht = perfectePlantSituatie[0] - 3
+            maximaleGrondvocht = perfectePlantSituatie[0] + 3
+            // minimale/maximale grondvocht waardes
+            minimaletemperatuur = perfectePlantSituatie[1] - 3
+            maximaletemperatuur = perfectePlantSituatie[1] + 3
+        }
+    }
+
+    // checks if plant is in CSV
+    if (perfectePlantSituatie.length == 0) {
+        console.log("plant niet gevonden in CSV")
+
+    } else {
+        meldingen()
+    }
+}
+
+// Message to user if no Bud has been connected yet
+function noPlantsMessage() {
+        let noPlants = document.createElement("p")
+        let noPlantsTextBig = document.createTextNode("Het lijkt erop dat je nog geen BUD hebt toegevoegd.")
+        let noPlantsText = document.createTextNode( "Click the button below to add a plant")
+        noPlants.appendChild(noPlantsText)
+        let noPlantsDiv = document.getElementById("no-plants")
+        noPlantsDiv.appendChild(noPlantsTextBig)
+        // noPlantsDiv.appendChild(noPlantsText)
+    }
+
+
+// Data output for user
+function meldingen() {
+    if (message == true) {
+        createElement();
+    }
+    
+    let meldingGrondvocht = () => {
+        if (grondvochtSensor >= minimaleGrondvocht && grondvochtSensor <= maximaleGrondvocht) {
+            message = true
+            document.querySelector("#sensor-bud").innerHTML = "Bud 1: "
+            console.log('alles gaat goed')
+        } else if (grondvochtSensor >= minimaleGrondvocht && grondvochtSensor >= maximaleGrondvocht) {
+            message = true
+            document.querySelector("#sensor-bud").innerHTML = "Bud 1: "
+            document.getElementById("vocht").innerHTML = "Je plant is aan het uitdrogen!";
+            console.log('je plant is aan het uitdrogen')
+        } else if (grondvochtSensor <= minimaleGrondvocht && grondvochtSensor <= maximaleGrondvocht) {
+            message = true
+            document.querySelector("#sensor-bud").innerHTML = "Bud 1: "
+            document.getElementById("vocht").innerHTML = "Je plant is te nat!";
+            console.log("Je plant is te nat!")
+        } else {
+        noPlantsMessage()
+            console.log('geen plant');
+        }
+    }
+    let meldingTemperatuur = () => {
+        if (temperatuurSensor >= minimaletemperatuur && temperatuurSensor <= maximaletemperatuur) {
+            document.querySelector("#sensor-bud").innerHTML = "Bud 1: "
+            message = true
+            console.log('alles gaat goed')
+        } else if (temperatuurSensor <= minimaletemperatuur && temperatuurSensor <= maximaletemperatuur) {
+            message = true
+            document.querySelector("#sensor-bud").innerHTML = "Bud 1: "
+            document.getElementById("temp").innerHTML = "Je plant bevriest zo wat!";
+            console.log('Je plant bevriest zowat!')
+        } else if (temperatuurSensor >= minimaletemperatuur && temperatuurSensor >= maximaletemperatuur) {
+            document.querySelector("#sensor-bud").innerHTML = "Bud 1: "
+            message = true
+            document.getElementById("temp").innerHTML = "Het is veeel te warm voor je plant!";
+            console.log("Het is veeel te warm voor je plant!")
+        } else {
+            console.log('geen plant');           
+        }
+    }
+    // document.querySelector("#naam").innerHTML = plantvoorspelling
+    // document.querySelector("#naam").element.style.color = red
+    console.log("grondvocht melding:")
+    meldingGrondvocht();
+    console.log('temperatuur melding:')
+    meldingTemperatuur();
+    // message = true
+}
+
+function createElement() {
+    // Create all output elements to display on the application
+    let output = document.createElement('div').classList.add('output');
+    let sensorBud = document.createElement('h3').classList.add('sensor_bud');
+    let naam = document.createElement('h3').classList.add('naam');
+    let vocht = document.createElement('p').classList.add('vocht');
+    let temp = document.createElement('p').classList.add('temp');
+
+    let outputDiv = document.getElementsByClassName('output_container')
+    outputDiv.appendChild(output);
+    output.appendChild(sensorBud, naam, vocht, temp);
+}
+
+getSensorData();
+window.localStorage.clear();
